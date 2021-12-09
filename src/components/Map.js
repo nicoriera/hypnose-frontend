41e79f100d715be87ffb176886ee31709154964a
nicoriera <80978348+nicoriera@users.noreply.github.com>
2021-12-09 15:38:54 +0100

    update map with marker and infowindow

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,5 +1,5 @@
 import React, {Component} from "react";
-import { Map, GoogleApiWrapper } from 'google-maps-react';
+import { Map, GoogleApiWrapper, InfoWindow, Marker} from 'google-maps-react';
 
 const mapStyles = {
   width: '30%',
@@ -8,9 +8,29 @@ const mapStyles = {
   marginLeft: '20px',
 };
 export class MapContainer extends Component {
+   state = {
+    showingInfoWindow: false,  // Hides or shows the InfoWindow
+    activeMarker: {},          // Shows the active marker upon click
+    selectedPlace: {}          // Shows the InfoWindow to the selected place upon a marker
+  };
+   onMarkerClick = (props, marker, e) =>
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true
+    });
+
+  onClose = props => {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      });
+    }
+  };
   render() {
     return (
-      <Map
+    <Map
         google={this.props.google}
         zoom={17}
         style={mapStyles}
@@ -20,7 +40,21 @@ export class MapContainer extends Component {
             lng: -1.7759
           }
         }
+      >
+      <Marker
+          onClick={this.onMarkerClick}
+          name={'Hypnothérapeute - Christphe Baccou'}
       />
+        <InfoWindow
+          marker={this.state.activeMarker}
+          visible={this.state.showingInfoWindow}
+          onClose={this.onClose}
+        >
+          <div>
+            <h4>{this.state.selectedPlace.name}</h4>
+          </div>
+        </InfoWindow>
+    </Map>
     );
   }
 }
