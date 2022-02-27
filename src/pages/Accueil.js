621783dee6e2599180892e65a42665b6f56cc17b
nicoriera <80978348+nicoriera@users.noreply.github.com>
2022-02-27 16:37:19 +0100

    delete fomrulaire contact

--- src/pages/Accueil.js
+++ src/pages/Accueil.js
@@ -1,8 +1,5 @@
-import React, { useRef }  from "react";
+import React from "react";
 import { Link } from "react-router-dom";
-import { Formik } from "formik";
-import * as Yup from "yup";
-import emailjs from "emailjs-com";
 
 // PICTURES
 import LogoPsynapse from "../assets/pictures/logo_psynapse.png";
@@ -19,43 +16,6 @@ import Button from "../components/Button"
 import Pourquoi from "../components/Pourquoi"
 
 const Accueil = () => {
-  const form = useRef();
-  const sendEmail = (e) => {
-    e.preventDefault();
-    emailjs
-      .sendForm(
-        "service_lk100qa",
-        "template_ek4pxmj",
-        form.current,
-        "user_cKq2Z3T1s3HXVMbKN6Cg6"
-      )
-      .then(
-        (result) => {
-          console.log(result.text);
-        },
-        (error) => {
-          console.log(error.text);
-        }
-      );
-  };
-
-  const initialValues = {
-    nom: "",
-    email: "",
-    demande: "",
-    message: "",
-  };
-
-  const validationSchema = Yup.object().shape({
-    nom: Yup.string().required("Obligatoire"),
-    email: Yup.string()
-      .email("Adresse e-mail non valide")
-      .required("Obligatoire"),
-    demande: Yup.string().required("Obligatoire"),
-    message: Yup.string()
-      .min(15, "15 caractères minimum ou plus")
-      .required("Obligatoire"),
-  });
   
   return (
     <div>
@@ -173,98 +133,6 @@ const Accueil = () => {
               <div><span className="bold">Horaires : </span><span>du mardi au vendredi en journée et le samedi matin</span></div>
             </div>
             <div><span className="bold">Infos : </span><span>Parking gratuit à Gaztelu Zahar (140 mètres) / Bus n°3 – Arrêt Ville (70 mètres) / Bus n°33-35-37 – Arrêt Mairie (220 mètres) </span></div>
-            </div>
-          <div className="accueil-container-contact-formulary">
-            <h3>Formulaire de contact</h3>
-            <Formik
-            initialValues={initialValues}
-            validationSchema={validationSchema}
-            onSubmit={(values, { setSubmitting, resetForm }) => {
-              setTimeout(() => {
-                alert(JSON.stringify(values, null, 2));
-                setSubmitting(false);
-              }, 400);
-              resetForm({ values: "" });
-              sendEmail();
-            }}
-            >
-            {({
-              values,
-              errors,
-              touched,
-              handleChange,
-              handleBlur,
-              handleSubmit,
-              isSubmitting,
-              /* and other goodies */
-            }) => (
-              <form
-                ref={form}
-                className="contact-formulary"
-                onSubmit={sendEmail}
-              >
-                <input
-                  id="nom"
-                  type="text"
-                  name="nom"
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  value={values.nom}
-                  placeholder="Votre nom"
-                />
-                <div className="error">
-                  {errors.nom && touched.nom && errors.nom}
-                </div>
-
-                <input
-                  id="email"
-                  type="email"
-                  name="email"
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  value={values.email}
-                  placeholder="Votre email"
-                />
-                <div className="error">
-                  {errors.email && touched.email && errors.email}
-                </div>
-
-                <input
-                  id="text"
-                  type="text"
-                  name="demande"
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  value={values.demande}
-                  placeholder="Votre demande concerne"
-                />
-                <div className="error">
-                  {errors.demande && touched.demande && errors.demande}
-                </div>
-
-                <textarea
-                  id="textaera"
-                  type="textaera"
-                  name="message"
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  value={values.message}
-                  placeholder="Votre message"
-                />
-                <div className="error">
-                  {errors.message && touched.message && errors.message}
-                </div>
-
-                <button
-                  className="button-contact"
-                  type="submit"
-                  disabled={isSubmitting}
-                >
-                  Envoyer
-                </button>
-              </form>
-              )}
-              </Formik>
             </div>
             </div>
           </div>
