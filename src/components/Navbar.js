ebe10dd05cd0e9a77839575717545512206d4673
Nicolas RIERA <nicolariera@gmail.com>
2022-12-15 14:38:12 +0100

    update css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,84 +1,74 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 const Navbar = () => {
-   const [sidebar, setSidebar] = useState(false)
+  const [sidebar, setSidebar] = useState(false);
 
-  const showSidebar = () => setSidebar(!sidebar)
+  const showSidebar = () => setSidebar(!sidebar);
 
   return (
-  <>
-    <nav className={sidebar ? 'nav-menu active' : 'nav-menu' }>
-      <ul className={sidebar ? 'menu active' : 'menu' }>
-        <li>
-          <Link className="link" to="/">
-            Accueil
-          </Link>
-        </li>
-        <li>
-          <Link className="link" to="/presentation">
-          <div>
-          L’hypnose,
-          </div>
-          <div>
-          c’est quoi ?
-          </div>
-          </Link>
-        </li>
-        <li>
-          <Link className="link" to="/seance">
-          <div>
-          Comment
-          </div>
-          <div>
-          se déroule une séance ?
-          </div>
-          </Link>
-        </li>
-        <li>
-          <Link className="link" to="/qui">
-          <div>
-          Votre
-          </div>
-          <div>
-          hypnothérapeute
-          </div>
-          </Link>
-        </li>
-        <li>
-          <Link className="link" to="/tarifs">
-          <div>
-          Quels
-          </div>
-          <div>
-          sont mes tarifs ?
-          </div>
-          </Link>
-        </li>
-        <li>
-          <Link className="link" to="/contact">
-            Contact
-          </Link>
-        </li>
-        <li>
-          <Link className="link-bold" to={{ pathname: "https://www.doctolib.fr/hypnotherapeute/hendaye/christophe-baccou" }} target="_blank">
-          <div>
-          Prendre
-          </div>
-          <div>
-          rendez-vous
-          </div>
-          </Link>
-        </li>
-      </ul>
-      <FontAwesomeIcon  onClick={showSidebar} className="logo-cross" icon="times" />
-    </nav>
-    
-    <div className="menu-toggle">
+    <>
+      <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
+        <ul className={sidebar ? "menu active" : "menu"}>
+          <li>
+            <Link className="link" to="/">
+              Accueil
+            </Link>
+          </li>
+          <li>
+            <Link className="link" to="/presentation">
+              L’hypnose, c’est quoi ?
+            </Link>
+          </li>
+          <li>
+            <Link className="link" to="/seance">
+              Comment se déroule une séance ?
+            </Link>
+          </li>
+          <li>
+            <Link className="link" to="/qui">
+              Votre hypnothérapeute
+            </Link>
+          </li>
+          <li>
+            <Link className="link" to="/tarifs">
+              Quels sont mes tarifs ?
+            </Link>
+          </li>
+          <li>
+            <Link className="link" to="/contact">
+              Contact
+            </Link>
+          </li>
+          <li>
+            <Link
+              className="link-bold"
+              to={{
+                pathname:
+                  "https://www.doctolib.fr/hypnotherapeute/hendaye/christophe-baccou",
+              }}
+              target="_blank"
+            >
+              Prendre rendez-vous
+            </Link>
+          </li>
+        </ul>
+        <FontAwesomeIcon
+          onClick={showSidebar}
+          className="logo-cross"
+          icon="times"
+        />
+      </nav>
+
+      <div className="menu-toggle">
         <p>MENU</p>
-        <FontAwesomeIcon onClick={showSidebar} className="logo-bars" icon="bars" />
+        <FontAwesomeIcon
+          onClick={showSidebar}
+          className="logo-bars"
+          icon="bars"
+        />
       </div>
-</>
+    </>
   );
 };
 
